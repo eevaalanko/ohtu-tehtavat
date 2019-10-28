1556968cbbf2964a297a9b8d2146c90c46847fe5
Eeva Alanko <eeva.alanko@siili.com>
2019-10-28 21:51:54 +0200

    exercise 15: implement interface Reader

--- viikko1/NhlStatistics1/src/main/java/ohtuesimerkki/PlayerReader.java
+++ viikko1/NhlStatistics1/src/main/java/ohtuesimerkki/PlayerReader.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class PlayerReader {
+public class PlayerReader implements Reader {
 
     private Scanner scanner;
 
@@ -18,12 +18,13 @@ public class PlayerReader {
         }
     }
 
+    @Override
     public List<Player> getPlayers() {
         ArrayList<Player> players = new ArrayList<Player>();
 
         while (scanner.hasNextLine()) {
-            String[] parts =  scanner.nextLine().split(";");            
-            
+            String[] parts =  scanner.nextLine().split(";");
+
             if (parts.length > 3) {
                 players.add(new Player(parts[0].trim(), parts[1], extractInt(parts[3]), extractInt(parts[4])));
             }
